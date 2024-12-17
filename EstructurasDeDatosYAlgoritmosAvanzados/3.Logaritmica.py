0e19cb55bacfb3dd4ea827f64c0ceb9200336efb
Isai <47548422+isaiisj@users.noreply.github.com>
2024-12-17 03:04:06 -0600

    se corrigieron errores en el código

--- EstructurasDeDatosYAlgoritmosAvanzados/3.Logaritmica.py
+++ EstructurasDeDatosYAlgoritmosAvanzados/3.Logaritmica.py
@@ -1,12 +1,12 @@
-def busqueda_binaria(lista):
+def busqueda_binaria(lista, target):
     apuntador_izquierdo = 0
-    apuntador_derecho = len(nums) - 1 
+    apuntador_derecho = len(lista) - 1 
     
     while apuntador_izquierdo <= apuntador_derecho:
         mitad = (apuntador_izquierdo+apuntador_derecho) // 2
-        if nums[mitad] == target:
+        if lista[mitad] == target:
             return mitad
-        elif nums[mitad] < target:
+        elif lista[mitad] < target:
             apuntador_izquierdo = mitad + 1
         else: 
             apuntador_derecho = mitad - 1
